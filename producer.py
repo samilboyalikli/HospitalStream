12df12d4dd0cddbd127d2d4e62cef1310f476063
Samil BOYALIKLI <sboyalikli@gmail.com>
2024-12-28 21:55:52 +0300

    I coded age assignment block, but I'm still working on age ranges.

--- producer.py
+++ producer.py
@@ -1,20 +1,31 @@
-import socket
+# For now, I will code the city and age assignment mechanisms. 
+
 import random
-import time
 
-host = "127.0.0.1"
-port = 5005
-start_time = time.time()
+cities = ["Houston","Dallas","Jersey City","Washington","Boston"]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    try: 
-        s.connect((host, port))
-        s.sendall("producer connected".encode('utf-8'))
-        while time.time() - start_time < 60:
-            number = random.randint(0,100)
-            message = f"{number}\n"
-            s.sendall(message.encode('utf-8'))
-            print(f"Sent: {number}")
-            time.sleep(1)
-    except Exception as e:
-        print(f"There is a problem. Problem is:\n{e}")
+random_age = random.randint(0,18)
+random_city = random.choice(cities)
+print(random_city, random_age)
+
+#--------------------------------------------------------------------
+# import socket
+# import time
+# 
+# host = "127.0.0.1"
+# port = 5005
+# start_time = time.time()
+# 
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     try: 
+#         s.connect((host, port))
+#         s.sendall("producer connected".encode('utf-8'))
+#         while time.time() - start_time < 60:
+#             number = random.randint(0,100)
+#             message = f"{number}\n"
+#             s.sendall(message.encode('utf-8'))
+#             print(f"Sent: {number}")
+#             time.sleep(1)
+#     except Exception as e:
+#         print(f"There is a problem. Problem is:\n{e}")
+#-------------------------------------------------------------------
