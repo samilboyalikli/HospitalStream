ef85962fbbadacc168e38827b456a66a1e298185
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-01-17 15:48:00 +0300

    I deleted some comment lines from consumer.

--- consumer.py
+++ consumer.py
@@ -1,21 +1,19 @@
 from kafka import KafkaConsumer
 import json
 
-# Kafka Consumer'ı oluştur
 consumer = KafkaConsumer(
-    'hospital_kafka',                # Tüketilecek Kafka konusu
-    bootstrap_servers='localhost:9092',  # Kafka broker adresi
-    auto_offset_reset='earliest',        # İlk mesajdan başlamak için
-    enable_auto_commit=True,             # Mesajları otomatik işaretle
-    group_id='python-consumer-group',    # Consumer grubu tanımı
-    value_deserializer=lambda v: json.loads(v.decode('utf-8'))  # JSON veriyi çöz
+    'hospital_kafka',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',      
+    enable_auto_commit=True,           
+    group_id='python-consumer-group',  
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))
 )
 
 print("Consumer started, waiting for messages...")
 
 try:
     for message in consumer:
-        # Mesaj verisini al ve işleyin
         print(f"Received message: {message.value}")
 except KeyboardInterrupt:
     print("Consumer stopped manually.")
