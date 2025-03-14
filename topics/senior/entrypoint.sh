feb5f6b1b6d7edd0a13c764b4b2b76bd60f2435b
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-03-14 23:25:43 +0300

    I wrote entrypoint for senior topic.

--- topics/senior/entrypoint.sh
+++ topics/senior/entrypoint.sh
@@ -1,1 +1,13 @@
-kafka_python_ng==2.2.3
+#!/bin/bash
+
+sleep 10
+
+if [ "$SPARK_MODE" = "master" ]; then
+  /opt/spark/bin/spark-class org.apache.spark.deploy.master.Master --host spark-master --port 7077 --webui-port 8080
+else
+  /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker $SPARK_MASTER_URL
+fi
+
+spark-submit --master local[*] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 main.py
+
+tail -f /dev/null
